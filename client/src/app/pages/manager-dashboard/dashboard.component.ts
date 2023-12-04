5e4008d946920202a1d0bfe2b0c1852baf25efda
amanopyadav <amanopyadav@gmail.com>
2023-12-04 15:48:51 +0530

    new

--- client/src/app/pages/manager-dashboard/dashboard.component.ts
+++ client/src/app/pages/manager-dashboard/dashboard.component.ts
@@ -1,12 +1,7 @@
-
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ManagerService } from './manager.service';
-import { ConstantPool } from '@angular/compiler';
-import { error } from 'console';
-
-
-
+import { EmployeeService } from 'app/employee.service';
 
 @Component({
   selector: 'app-user',
@@ -59,10 +54,7 @@ export class DashboardComponent implements OnInit {
       dob: [{ value: '', disabled: true }],
       joiningDate: [{ value: '', disabled: true }],
 
-      // project_name: ['', Validators.required],
-      // project_id: ['', Validators.required],
-      // client: ['', Validators.required],
-      // industry_name: ['', Validators.required],
+    
     });
 
     this.projectForm = this.fb.group({
@@ -95,9 +87,7 @@ export class DashboardComponent implements OnInit {
       }
     );
 
-    // Initialize filteredEmployees with all employees
-    // this.filteredEmployees = this.Employees;
-    // this.filteredProjects = this.Projects;
+  
   }
 
   openModal() {
@@ -109,23 +99,23 @@ export class DashboardComponent implements OnInit {
     this.display = "block";
   }
 
-  openModalforproj(){
-    console.log("Proj dialogbox opened");
-    this.fetchAllProjects();
+  // openModalforproj(){
+  //   console.log("Proj dialogbox opened");
+  //   this.fetchAllProjects();
 
-    this.display = "block";
-  }
+  //   this.display = "block";
+  // }
 
   onCloseHandled() {
     this.display = "none";
     this.searchData = ''; 
   }
 
-  onCloseHandledforProj(){
-    console.log("close proj dialog box");
-    this.display = "none";
-    this.searchData = ''; 
-  }
+  // onCloseHandledforProj(){
+  //   console.log("close proj dialog box");
+  //   this.display = "none";
+  //   this.searchData = ''; 
+  // }
 
   fetchAllEmployees() {
     this.managerService.getEmployees().subscribe(
@@ -139,38 +129,116 @@ export class DashboardComponent implements OnInit {
     );
   }
 
-  fetchAllProjects() {
-    this.managerService.getProject().subscribe(
-      (data) => {
-        this.filteredProjects = data;
-        console.log(data);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-}
-
-  // fetchData() {
-  //   this.managerService.getAllEmployees().subscribe(
-  //     (data: any[]) => {
-  //       this.apiData = data;
-  //       console.log('Fetched Data:', data);
+  // fetchAllProjects() {
+  //   this.managerService.getProject().subscribe(
+  //     (data) => {
+  //       this.filteredProjects = data;
+  //       console.log(data);
   //     },
   //     (error) => {
-  //       console.error('Error fetching data:', error);
+  //       console.error(error);
   //     }
   //   );
   // }
 
-  
-//     applyFilter() {
-//       this.filteredEmployeeData = this.employeeTableData.dataRows.filter(row =>
-//         Object.values(row).some(value =>
-//           value.toString().toLowerCase().includes(this.searchTerm.toLowerCase())
-//         )
-//       );
-//     }
+  addEmployee(employee: any) {
+    console.log("Checking!!");
+    console.log(employee.emp_id);
 
-// }
+    this.employeeService.setEmployeeId(employee.emp_id);
+  
+    const employeeFormValues = {
+      emp_id: employee.emp_id,
+      empName: employee.emp_name,
+      empDesignation: employee.designation_name,
+      function_name: employee.function_name,
+      primarySkillName: employee.primary_skill_name,
+      mindcraftExpMon: employee.mindcraft_exp_mon,
+      totalExpMon: employee.total_exp_mon,
+      email: employee.email,
+      mobileNo: employee.mobileno,
+      dob: employee.dob,
+      joiningDate: employee.joining_date,
+    };
+    this.nominationForm.patchValue(employeeFormValues);
+
+    this.onCloseHandled();
+  }
+
+  
+  
+  
+
+  onEmployeeIdChange() {
+    const empId = this.nominationForm.get('emp_id').value;
+
+    if (empId) {
+      this.fetchEmployeeDetails(empId);
+    }
+  }
+
+  
+  fetchEmployeeDetails(empId: number) {
+    this.managerService.getEmployeesDetails(empId).subscribe(
+      (response: any) => {
+        console.log(response);
+  
+        // Update the following lines based on the actual response structure
+        this.empDesignation = response.empDesignation;
+        this.function_name = response.unit;
+  
+        this.nominationForm.get('designation_name').setValue(this.empDesignation);
+        this.nominationForm.get('function_name').setValue(this.function_name);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+ 
+  search() {
+    console.log('Search function triggered');
+    console.log('Search Data:', this.searchData);
+    console.log('Employees:', this.Employees);
+
+    if (!this.Employees) {
+      return;
+    }
+
+    // Handle both scenarios: searching and showing all employees
+    this.filteredEmployees = this.searchData.trim() === ''
+      ? this.Employees // Show all employees
+      : this.Employees.filter(employee =>
+        employee.emp_name && employee.emp_name.toLowerCase().includes(this.searchData.toLowerCase())
+      );
+  }
+
+  
+ 
+
+addEmp() {
+  // Add your form values to addedEmployees array
+  this.addedEmployees.push({
+    empCode: this.nominationForm.get('emp_id').value,
+    empName: this.nominationForm.get('empName').value,
+    // Add other fields accordingly
+  });
+
+  // Clear the form or reset as needed
+  this.nominationForm.reset();
+}
+
+
+
+removeEmployee(employee: any) {
+  const index = this.addedEmployees.indexOf(employee);
+  if (index !== -1) {
+    this.addedEmployees.splice(index, 1);
+  }
+}
+
+
+  
+  
+}
